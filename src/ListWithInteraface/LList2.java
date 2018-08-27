195cdbb4d971bd3282fc3d66e30b6383682f2515
Andrew <osetsky.ay@gmail.com>
2018-08-28 01:46:47 +0300

    New List

---
+++ src/ListWithInteraface/LList2.java
@@ -0,0 +1,125 @@
+package ListWithInteraface;
+
+public class LList2 implements Elist {
+
+	Node root = null;
+	Node end = null;
+	class Node
+	{
+		
+	}
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init(int[] ini) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int[] toArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void addStart(int val) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addEnd(int val) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addPos(int pos, int val) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int delStart() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delEnd() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delPos(int pos) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int min() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int max() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int minPos() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int maxPos() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void sort() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void revers() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void halfRevers() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int get(int pos) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void set(int pos, int val) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
